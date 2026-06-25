9a427f8156036b460c366d78e737c9e0d0c98000
Arthur Augusto Rebelo Machado <arthurremac@gmail.com>
2026-06-25 20:08:18 -0300

    Ajuste de Pastas

--- Csharp/ProjetoDesafio/obj/Debug/net10.0/ProjetoDesafio.AssemblyInfo.cs
+++ Csharp/ProjetoDesafio/obj/Debug/net10.0/ProjetoDesafio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoDesafio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea50f12c06b49fd90dba889b54cb2745f5c1856")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d85b8136dcc221c13a66e8bbfa1c177f872757d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoDesafio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoDesafio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
