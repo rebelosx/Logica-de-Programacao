8652edef156f7129bcd99d8c7ad831cc3ea4fd3d
Arthur Augusto Rebelo Machado <arthurremac@gmail.com>
2026-09-16 22:39:25 -0300

    Aula 16/09/2026

--- Csharp/ProjetoDesafio/obj/Debug/net10.0/ProjetoDesafio.AssemblyInfo.cs
+++ Csharp/ProjetoDesafio/obj/Debug/net10.0/ProjetoDesafio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoDesafio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a1c86c92ad24e822c849033649e3e20e92df02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f06fa355eef569f649e405057cc6354b2538002")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoDesafio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoDesafio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
