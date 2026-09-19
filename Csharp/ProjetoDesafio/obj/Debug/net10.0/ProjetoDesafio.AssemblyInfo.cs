f2bb40ea1a74fc215047f7b8180a0a54426cf2e4
Arthur Augusto Rebelo Machado <arthurremac@gmail.com>
2026-09-18 21:06:54 -0300

    Aula 18/09/2026

--- Csharp/ProjetoDesafio/obj/Debug/net10.0/ProjetoDesafio.AssemblyInfo.cs
+++ Csharp/ProjetoDesafio/obj/Debug/net10.0/ProjetoDesafio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoDesafio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f06fa355eef569f649e405057cc6354b2538002")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8652edef156f7129bcd99d8c7ad831cc3ea4fd3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoDesafio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoDesafio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ HTML-CSS-JAVASCRIPT/aula11_exemplo1.js
@@ -0,0 +1,10 @@
+var idade = prompt("Digite sua idade: ")
+idade = parseInt(idade)
+
+if (idade > 18){
+    document.write("Você é maior de idade :)")
+} else if (idade = 18){
+    document.write("Você tem 18 anos.")
+} else {
+    document.write("Você é menor de idade :(")
+}
